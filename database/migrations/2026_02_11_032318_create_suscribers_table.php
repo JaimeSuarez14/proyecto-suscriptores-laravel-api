a508c13c386c01ab62c1daeb732d32b6f6416a7a
Jaime Suarez <jaisua8@gmail.com>
2026-02-10 22:29:41 -0500

    creacion de la migracion

--- database/migrations/2026_02_11_032318_create_suscribers_table.php
+++ database/migrations/2026_02_11_032318_create_suscribers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('suscribers', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('email', 50)->unique();
+            $table->boolean('active')->default(true);
             $table->timestamps();
         });
     }
